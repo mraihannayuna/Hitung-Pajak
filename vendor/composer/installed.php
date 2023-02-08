518179e88c794c09cff9b3200b33004517668d4c
mraihannayuna <m.raihannayua13@gmail.com>
2023-02-08 08:42:07 +0700

    PajakPPH21

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'hitung/pph21',
+        'name' => 'nayuna/pajakpph21',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a779efecf6045f23edf6635720b822ba02cced8a',
+        'reference' => '1411012ed71125538643423ffb9b02ef0cfe8318',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'hitung/pph21' => array(
+        'nayuna/pajakpph21' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a779efecf6045f23edf6635720b822ba02cced8a',
+            'reference' => '1411012ed71125538643423ffb9b02ef0cfe8318',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
